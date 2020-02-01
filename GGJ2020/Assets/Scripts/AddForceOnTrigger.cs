21d3e27acf8ac84d6a77e655b5cd62680389bf15
daquinte <daquinte@ucm.es>
2020-02-01 18:26:32 +0000

    El vapor te impulsaaa

---
+++ GGJ2020/Assets/Scripts/AddForceOnTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceOnTrigger : MonoBehaviour
+{
+    public float force = 10f;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerMovement pm = other.gameObject.GetComponent<PlayerMovement>();
+        if ( pm != null)
+        {
+            other.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, force), ForceMode2D.Impulse);
+        }
+    }
+}
