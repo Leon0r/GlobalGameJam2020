4f8c38f4dc203300ff01a8994e5f5558ae0cf754
Nope <leonorcu@ucm.es>
2020-02-01 02:59:47 +0100

    SCRIPT TEST SONIDO

--- GGJ2020/Assets/Scripts/test.cs
+++ GGJ2020/Assets/Scripts/test.cs
@@ -8,7 +8,11 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        AkSoundEngine.SetRTPCValue();       
+        /*
+         * Esta linea es para hacer el set de los parametros que se envian a sonido
+         * CUIDADO QUE LINEAS DE CODIGO RELACIONADO CON WWISE QUE NO COMPILE HACE QUE WWISE ENTERO SE DESCONECTE
+         */
+       // AkSoundEngine.SetRTPCValue("nombreParametro", valor);       
     }
 
     // Update is called once per frame
