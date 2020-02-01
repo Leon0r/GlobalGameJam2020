49a21628f55fb68b4a0e8af99689befa796de63e
adralcan <adrian97ad@gmail.com>
2020-02-01 15:30:10 +0100

    Controles de la cámara

---
+++ GGJ2020/Assets/Scripts/Camera/AuxiliarPlayerMoveManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(CameraController))]
+[RequireComponent (typeof(ReturnHome))]
+public class AuxiliarPlayerMoveManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool movingX;
+    bool movingY;
+
+    CameraController cameraController;
+    ReturnHome returnHome;
+    void Start() {
+        returnHome = GetComponent<ReturnHome>();
+        cameraController = GetComponent<CameraController>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        movingX = (Input.GetAxisRaw("Horizontal") != 0);
+        movingY = (Input.GetAxisRaw("Vertical") != 0);
+        if (!movingX && !movingY) {
+            returnHome.Move();
+        }
+        else {
+            cameraController.Move();
+        }
+    }
+}
---
+++ GGJ2020/Assets/Scripts/Camera/ReturnHome.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnHome : MonoBehaviour
+{
+
+    Vector3 startPosition, driftPosition;
+    private Rigidbody rb;
+
+    public float driftSeconds = 3.0f;
+    private float driftTimer = 0;
+    public float speed = 10.0f;
+    private bool isDrifting = false;
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        startPosition = Vector3.zero;
+    }
+
+
+    private void StartDrift() {
+        isDrifting = true;
+        driftTimer = 0;
+        driftPosition = transform.position;
+        rb.velocity = Vector3.zero;
+    }
+
+    private void StopDrift() {
+        isDrifting = false;
+        transform.position = startPosition;
+        rb.velocity = Vector3.zero;
+    }
+
+    public void Move()
+    {
+        StartDrift();
+        if (isDrifting) {
+            driftTimer += Time.deltaTime * speed;
+            if (driftTimer > driftSeconds) {
+                StopDrift();
+            }
+            else {
+                float ratio = driftTimer / driftSeconds;
+                transform.position = Vector3.Lerp(driftPosition, startPosition, ratio);
+            }
+        }
+    }
+}
