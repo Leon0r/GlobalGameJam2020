e2c80f350c45eaffec50508ba11990f8475de3b5
Nope <leonorcu@ucm.es>
2020-02-01 02:51:14 +0100

    sound test script

---
+++ GGJ2020/Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AkSoundEngine.SetRTPCValue();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
