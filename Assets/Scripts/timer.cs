5c646287ad871140a72dbf0d68a948ca70af0c65
adralcan <adrian97ad@gmail.com>
2020-02-02 11:09:54 +0100

    Timer para la mecánica principal

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Image fillImg;
+    public float countdown = 10;
+    private float currentTime;
+
+
+    // Start is called before the first frame update
+    void Start() {
+        fillImg = GetComponent<Image>();
+        currentTime = countdown;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentTime > 0) {
+            currentTime -= Time.deltaTime;
+            fillImg.fillAmount = currentTime / countdown;
+        }
+        else {
+            currentTime = countdown;
+        }
+    }
+}
